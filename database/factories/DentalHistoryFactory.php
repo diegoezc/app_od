5f92b0ce15b987a9664538c57ba424ecd8d71616
ailec alviarez <alviarezailec@gmail.com>
2022-05-13 00:19:33 -0400

    creacion de factories, se termino user, user_detail

---
+++ database/factories/DentalHistoryFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\DetailFather;
+use App\Models\DetailMother;
+use App\Models\Location;
+use App\Models\Referred;
+use App\Models\Sector;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class DentalHistoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'date' => $this->faker->date(),
+            'location_id' => Location::factory()->create()->id,
+            'sector_id' => Sector::factory()->create(),id,
+            'user_id' => User::factory()->create(),id,
+            'referred_id' => Referred::factory()->create(),id,
+            'detail_mother_id' => DetailMother::factory()->create(),id,
+            'detail_father_id' => DetailFather::factory()->create(),id,
+        ];
+    }
+}
