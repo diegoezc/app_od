58873dc22f059afa3fabfd3ad8c27824b250b883
maria de los angeles <mariaramireeez2002@gmail.com>
2022-05-21 10:23:07 -0400

    update

--- app/Interfaces/DetailParent/DetailParentInterface.php
+++ app/Interfaces/DetailParent/DetailParentInterface.php
@@ -7,9 +7,11 @@ interface DetailParentInterface
     const NAME = 'name';
     const OCCUPATION_ID = 'occupation_id';
     const BUSINESS = 'business';
+    const PHONE_NUMBER='phone_number';
     const FILLABLE =[
         self::NAME,
         self::OCCUPATION_ID,
-        self::BUSINESS
+        self::BUSINESS,
+        self::PHONE_NUMBER
     ];
 }
---
+++ database/migrations/2022_05_21_141822_add_column_phone_number_in_detail_father.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnPhoneNumberInDetailFather extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('detail_fathers', function (Blueprint $table) {
+            if(Schema::hasColumn('detail_fathers','phone_number')){
+                $table->dropColumn('phone_number')->nullable();
+
+            }
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('detail_fathers', function (Blueprint $table) {
+            if(Schema::hasColumn('detail_fathers','phone_number')){
+                $table->dropColumn(['phone_number']);
+
+            }
+        });
+    }
+}
